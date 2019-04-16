51e65a84c6dd5eaff54a09503cd7981dab38fede
Paolo Sandoval <paolo.jalasoft@jalasoft.com>
2019-04-15 21:44:09 -0400

    Add identation

--- frontend/src/app/components/users/list_users/list.users.controller.js
+++ frontend/src/app/components/users/list_users/list.users.controller.js
@@ -9,15 +9,15 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
     .controller('ListUsersController', ['$scope', '$mdToast', '$http', function($scope, $mdToast, $http) {
         $scope.employees = [];
         $scope.showAllEmployee = function() {
-          $http({
-              method: 'get', 
-              url: 'http://localhost:3000/crud/employees'
-          }).then(function (response) {
-              console.log(response, 'res');
-              $scope.employees = response.data;
-          },function (error){
-              console.log(error, 'can not get data.');
-          });
+            $http({
+                method: 'get', 
+                url: 'http://localhost:3000/crud/employees'
+            }).then(function (response) {
+                console.log(response, 'res');
+                $scope.employees = response.data;
+            }, function (error) {
+                console.log(error, 'can not get data.');
+            });
         };
 
         $scope.addEmployee = function() {
@@ -27,8 +27,6 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
             fd.append('lastName', $scope.lastName);
             fd.append('age', $scope.age);   
             fd.append('birthdate', moment($scope.birthdate).format("YYYY-MM-DD"));   
-            console.log($scope.birthdate);
-            console.log(fd);
             $http({
                 method: 'post', 
                 url: 'http://localhost:3000/crud/employees',
@@ -40,9 +38,8 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
                 $scope.clearModel();
                 $scope.showAllEmployee();
             },function (error){
-              $scope.showMessage('can not update data.');
+                $scope.showMessage('can not update data.');
             });
-
         };
 
         $scope.selectedEmployee = function(employee) {
@@ -60,28 +57,26 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
         }
 
         $scope.editEmployee = function() {
-          const fd = new FormData(); 
-          fd.append('imageUpload', $scope.selectedFile);
-          fd.append('firstName', $scope.firstName);
-          fd.append('lastName', $scope.lastName);
-          fd.append('age', $scope.age);   
-          fd.append('birthdate', moment($scope.birthdate).format("YYYY-MM-DD"));   
+            const fd = new FormData(); 
+            fd.append('imageUpload', $scope.selectedFile);
+            fd.append('firstName', $scope.firstName);
+            fd.append('lastName', $scope.lastName);
+            fd.append('age', $scope.age);   
+            fd.append('birthdate', moment($scope.birthdate).format("YYYY-MM-DD"));   
 
-          console.log(fd);
-          $http({
-              method: 'put', 
-              url: 'http://localhost:3000/crud/employees/' + $scope.selectedEmployee._id,
-              headers: { 'Content-Type': undefined },
-              data: fd
-          }).then(function (response) {
-              $scope.clearModel();
-              $scope.showMessage('Updated Data.');
-              $scope.showAllEmployee();
-          },function (error){
-               $scope.showMessage('can not update data.');
-          });
-
-      };
+            $http({
+                method: 'put', 
+                url: 'http://localhost:3000/crud/employees/' + $scope.selectedEmployee._id,
+                headers: { 'Content-Type': undefined },
+                data: fd
+            }).then(function (response) {
+                $scope.clearModel();
+                $scope.showMessage('Updated Data.');
+                $scope.showAllEmployee();
+            },function (error){
+                $scope.showMessage('can not update data.');
+            });
+        };
 
         $scope.deleteEmployee = function (employee) {
             $http({
@@ -109,10 +104,10 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
         };
 
         $scope.showMessage = function(message) {
-          $mdToast.show(
-            $mdToast.simple()
-              .textContent(message)
-              .position('top')
+            $mdToast.show(
+                $mdToast.simple()
+                .textContent(message)
+                .position('top')
             ); 
         };
         $scope.showAllEmployee();
@@ -120,39 +115,38 @@ const module = angular.module('list.users.controllers', ['ngMaterial'])
     .directive('apsUploadFile', apsUploadFile);
 
 function apsUploadFile() {
-  var directive = {
-    restrict: 'E',
-    template: '<input id="fileInput" type="file" class="ng-hide"> <md-button id="uploadButton" class="md-raised md-primary" aria-label="attach_file">    Choose image file </md-button><md-input-container  md-no-float flex="50">    <input id="textInput" ng-model="fileName" type="text" placeholder="No file chosen" ng-readonly="true"></md-input-container>',
-    link: apsUploadFileLink
-  };
-  return directive;
+    var directive = {
+        restrict: 'E',
+        template: '<input id="fileInput" type="file" class="ng-hide"> <md-button id="uploadButton" class="md-raised md-primary" aria-label="attach_file">    Choose image file </md-button><md-input-container  md-no-float flex="50">    <input id="textInput" ng-model="fileName" type="text" placeholder="No file chosen" ng-readonly="true"></md-input-container>',
+        link: apsUploadFileLink
+    };
+    return directive;
 }
 
 function apsUploadFileLink(scope, element, attrs) {
-  var input = $(element[0].querySelector('#fileInput'));
-  var button = $(element[0].querySelector('#uploadButton'));
-  var textInput = $(element[0].querySelector('#textInput'));
+    var input = $(element[0].querySelector('#fileInput'));
+    var button = $(element[0].querySelector('#uploadButton'));
+    var textInput = $(element[0].querySelector('#textInput'));
 
-  if (input.length && button.length && textInput.length) {
-    button.click(function(e) {
-      input.click();
+    if (input.length && button.length && textInput.length) {
+        button.click(function(e) {
+        input.click();
     });
     textInput.click(function(e) {
-      input.click();
+        input.click();
     });
-  }
+}
 
-  input.on('change', function(e) {
-    var files = e.target.files;
-    if (files[0]) {
-      scope.selectedFile = files[0];
-      console.log(scope.selectedFile);
-      scope.fileName = files[0].name;
-    } else {
-      scope.fileName = null;
-    }
-    scope.$apply();
-  });
+    input.on('change', function(e) {
+        var files = e.target.files;
+        if (files[0]) {
+            scope.selectedFile = files[0];
+            scope.fileName = files[0].name;
+        } else {
+            scope.fileName = null;
+        }
+        scope.$apply();
+    });
 }
 
 export default module;
