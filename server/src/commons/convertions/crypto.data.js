3e6ebf335f968b6d1b37017b5e107100f0617b77
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2019-05-07 18:05:45 -0400

    fix some eslint validation

--- server/src/commons/convertions/crypto.data.js
+++ server/src/commons/convertions/crypto.data.js
@@ -4,19 +4,19 @@ import fs from 'fs';
 export default class cryptoFile {
     getCheckSum(filePath, algorithm) {
         return new Promise((resolve, reject) => {
-            let shasum = crypto.createHash(algorithm || 'md5');
+            const shasum = crypto.createHash(algorithm || 'md5');
             try {
-                let s = fs.ReadStream(filePath)
-                s.on('data', function (data) {
-                    shasum.update(data)
-                })
-                s.on('end', function () {
-                    const hash = shasum.digest('hex')
-                   return resolve(hash);
-                })
+                const s = fs.ReadStream(filePath);
+                s.on('data', (data) => {
+                    shasum.update(data);
+                });
+                s.on('end', () => {
+                    const hash = shasum.digest('hex');
+                    return resolve(hash);
+                });
             } catch (error) {
-                return reject('calc fail');
+                return reject(new Error('calc fail'));
             }
         });
-      }
+    }
 }
--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 import config from '../config/environment';
 import message from '../commons/constants/messages';
 
@@ -7,11 +7,11 @@ export default class DataBaseConnection {
     constructor() {
         this.URI = `${config.uri}${config.db}`;
     }
-    
+
     getDataBaseConnection() {
         mongoose.connect(this.URI)
-            .then(db => console.log(message.dbConnected))
-            .catch(err => console.log(err));
+        .then(() => console.info(message.dbConnected))
+        .catch((error) => console.error(error));
         return mongoose;
     }
 }
