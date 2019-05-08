0d59407b4a489a5a7d5daad3a1300fd59765906e
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2019-05-08 11:59:47 -0400

    move directive's logic to controller

--- frontend/src/app/components/users/birthday/users.birthday.controller.js
+++ frontend/src/app/components/users/birthday/users.birthday.controller.js
@@ -1,5 +1,6 @@
 
 import config from '../../../../config/environment';
+import defaultImg from '../../../../resources/default.jpg';
 
 /**
  * Controller for Birthday
@@ -15,6 +16,7 @@ export default class BirthdayController {
         this.employeeLastName = this.$routeParams.lastName;
         this.employeePhotoUrl = this.$routeParams.photoUrl;
         this.employees = [];
+        this.defaultImg = defaultImg;
         this.baseUri = `http://${config.serverHost}:${config.serverPort}`;
         this.employeeRoute = `${this.baseUri}/crud/employees`;
         if (!this.employeeId) {
@@ -40,23 +42,49 @@ export default class BirthdayController {
             method: 'get', 
             url: this.urlBirthday
         }).then((response) => {
-            this.employeeImg = this.defaultImg;
             if (Array.isArray(response.data) && response.data.length > 0) {
                 this.employees = response.data;
                 let index = this.employees.length - 1;
                 this.employeeName = this.employees[index].firstName + ' ' + this.employees[index].lastName;
                 this.employeeImg = this.employees[index].image;
+                this.showEmployee();
             } else {
                 if (Object.keys(response.data).length > 0) {
                     this.employee = response.data;
                     this.employeeName = this.employee.firstName + ' ' + this.employee.lastName;
                     this.employeeImg = this.employee.image;
                 } else {
+                    this.baseUri = "";
                     this.employeeImg = this.defaultImg;
                 }
             }
         }, (error) => {
             console.error(error, 'can not get data.');
         });
+    }
+
+    /**
+     * Method to show the employees list by interval.
+     */
+    showEmployee() {
+        let index = 0; 
+        if (this.employees.length > 1) {
+            setInterval(() => {
+                if(this.employees.length === 0) {
+                    return;
+                }
+                const name = document.getElementById("employeeName");
+                name.textContent = this.employees[index].firstName + ' ' + this.employees[index].lastName;
+                const image = document.getElementById("employeeImageList");
+                image.src = this.baseUri + this.employees[index].image;
+                index++;
+                if(index === this.employees.length) {
+                    index = 0;
+                }
+            }, 6000);
+        } else {
+            return;
+        } 
+        
     }
 }
--- frontend/src/app/components/users/birthday/users.birthday.module.js
+++ frontend/src/app/components/users/birthday/users.birthday.module.js
@@ -1,10 +1,8 @@
 import BirthdayController from './users.birthday.controller';
-import ImageInterval from './image.interval.directive';
 
 const birthdayModule = angular.module('user.birthday.controllers', [
 ]);
 
 birthdayModule.controller('BirthdayController', BirthdayController);
-birthdayModule.directive('imageInterval', ImageInterval);
 
 export default birthdayModule;
